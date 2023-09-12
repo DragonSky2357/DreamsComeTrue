800e4dc20b3810e81473f5cad1d3367192f9b9c7
dragonsky2357 <qkrdydals327@gmail.com>
2023-09-12 11:07:23 +0900

    REFACTOR: uploadfile function

--- backend/src/common/utils/multer.options.ts
+++ backend/src/common/utils/multer.options.ts
@@ -1,29 +1,30 @@
-import { ConfigService } from '@nestjs/config';
 import { S3Client } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import multerS3 from 'multer-s3';
 import path from 'path';
-import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import { v4 as uuid } from 'uuid';
 
 export const multerOptionsFactory = (
-  configservice: ConfigService,
+  configService: ConfigService,
 ): MulterOptions => {
   const s3 = new S3Client({
-    region: configservice.get('AWS_BUCKET_REGION'),
+    region: configService.get('AWS_BUCKET_REGION'),
     credentials: {
-      accessKeyId: configservice.get('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: configservice.get('AWS_SECRET_ACCESS_KEY'),
+      accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
     },
   });
 
   return {
     storage: multerS3({
       s3,
-      bucket: configservice.get('AWS_S3_BUCKET_NAME'),
-      key(_req, file, done) {
+      bucket: configService.get('AWS_S3_BUCKET_NAME'),
+      key(req, file, done) {
         const ext = path.extname(file.originalname);
-        const basename = path.basename(file.originalname, ext);
 
-        done(null, `${basename}_${Date.now()}${ext}`);
+        console.log(`${uuid()}${ext}`);
+        done(null, `${uuid()}${ext}`);
       },
     }),
     limits: { fileSize: 10 * 1024 * 1024 },
--- backend/src/common/utils/utils.ts
+++ backend/src/common/utils/utils.ts
@@ -1,4 +1,6 @@
 import * as bcrypt from 'bcrypt';
+import Aws, { S3 } from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
 
 export const hash = async (plainText: string): Promise<string> => {
   const saltOrRounds = 10;
@@ -7,4 +9,27 @@ export const hash = async (plainText: string): Promise<string> => {
 
 export const isHashValid = async (password, hashPassword): Promise<boolean> => {
   return await bcrypt.compare(password, hashPassword);
+};
+
+export const uploadFile = async (file: any, folder: string) => {
+  const s3 = new Aws.S3({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+  try {
+    return await s3
+      .upload({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `${folder}/${uuidv4()}.png`,
+        Body: file,
+        ContentType: 'Content-Type: image/png',
+      })
+      .promise();
+  } catch (err) {
+    console.error(err);
+  }
 };
