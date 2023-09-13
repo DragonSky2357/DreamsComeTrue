e70d70426b1b206257cc485c553b741cc749d1e4
dragonsky2357 <qkrdydals327@gmail.com>
2023-09-14 00:15:40 +0900

    feat: debouncedSearch

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,30 +1,89 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+
+import PrimarySearchAppBar from "../components/TitleBar";
+import { Container, IconButton, Skeleton, TextField } from "@mui/material";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
 import Mainboard from "../components/Mainbord";
-import PrimarySearchAppBar from "../components/TitleBar/TitleBar";
+import { debounce } from "lodash";
+
+const Wrapper = styled.div`
+  margin: 100px;
+`;
+const SearchWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  width: 60%;
+  background-color: #ffffff;
+`;
+
+const ContentsWrapper = styled.div`
+  margin-top: 100px;
+`;
 
 const SearchPage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = useState("");
+
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const search = searchParams.get("s");
+  const debouncedSearch = useCallback(
+    debounce(async (search) => {
+      try {
+        axios
+          .get(`${process.env.REACT_APP_BASE_URL}/post/search`, {
+            params: { title: search, content: search },
+          })
+          .then((response: any) => {
+            setPosts(response.data);
+            console.log(response.data);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    }, 500),
+    []
+  );
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BASE_URL}/post/search`, {
-        params: { title: search, content: search },
-      })
-      .then((response: any) => {
-        setPosts(response.data);
-      });
-  }, [search]);
+  const handleSearchChange = (event: any) => {
+    const inputSearchTerm = event.target.value;
+    setSearch(inputSearchTerm);
+
+    // Debounce를 적용한 검색 함수를 호출합니다.
+    debouncedSearch(inputSearchTerm);
+  };
 
   return (
-    <div>
+    <>
       <PrimarySearchAppBar />
-      <Mainboard posts={posts} />
-    </div>
+      <Wrapper>
+        <SearchWrapper>
+          <SearchIcon
+            style={{
+              padding: "10px",
+              width: "50px",
+              height: "50px",
+              color: "#000000",
+            }}
+          />
+          <TextField
+            type="search"
+            id="search"
+            fullWidth
+            autoFocus
+            InputProps={{ style: { fontSize: 30, border: "none" } }}
+            onChange={handleSearchChange}
+          />
+        </SearchWrapper>
+        <ContentsWrapper>
+          {loading && <Skeleton width={210} height={118} />}
+          <Mainboard posts={posts} />
+        </ContentsWrapper>
+      </Wrapper>
+    </>
   );
 };
 
